94f0b6490fa494feb5dc38e706c7d3777f2648ed
Prabhat Kumar <prabhssgg3@gmail.com>
2023-02-23 23:33:25 +0530

    day3work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Box } from "@chakra-ui/layout";
+import GalleryCarousel from "./components/carousel";
+import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <GalleryCarousel />
+      <Box height={"100vh"}>
+        <Footer />
+      </Box>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "react-multi-carousel/lib/styles.css";
+
 // import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { ChakraProvider } from "@chakra-ui/react";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider>
     <App />
   </ChakraProvider>
-)
+);
